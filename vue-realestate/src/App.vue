fa68dd2d99068fac0cf4613577c900c654f9ee78
danteparra <dantelean.parra@fujitsu.com>
2024-11-22 09:52:54 +0800

    Push1: Navbar creation

--- vue-realestate/src/App.vue
+++ vue-realestate/src/App.vue
@@ -1,14 +1,60 @@
 <script setup>
-import HelloWorld from './components/HelloWorld.vue'
-import TheWelcome from './components/TheWelcome.vue'
+
 </script>
 
 <template>
-asdf
 
-<p class="text-xl italic text-green-500">
-  tite
-</p>
+<div class="container mx-auto px-6 lg:px-20">
+
+  <div id="Navbar" class="flex flex-row my-4">
+    <div class="mr-auto">
+      <div class="py-2 px-4 border-[3px] rounded-full border-slate-600 text-slate-600 text-lg font-extrabold">
+        <i class="pi pi-home mr-2 text-2xl align-text-bottom text-teal-500"></i>Real Estate Godz
+      </div>
+    </div>
+    
+    <div class="flex flex-row">
+      <div class="self-center">
+        <a href="#" class="nav-link text-teal-500">
+          Home
+        </a>
+        <a href="#" class="nav-link">
+          About
+        </a>
+        <a href="#" class="nav-link">
+          Properties
+        </a>
+        <a href="#" class="nav-link">
+          Sell your home
+        </a>
+        <a href="#" class="nav-link">
+          Contact
+        </a>
+      </div>
+      <div class="border-r-2 border-slate-600 mx-3"></div>
+      <div class="self-center p-3 text-slate-600 text-sm">
+        <span><i class="pi pi-phone text-xs"></i> Call:</span>
+        <a href="#" class="text-slate-600 transition hover:text-teal-500">
+          1234-123-1234
+        </a>
+      </div>
+      <div class="self-center">
+        <a href="#" class="border-2 py-2 px-4 pr-3 border-slate-600 rounded-full text-slate-600 text-sm font-bold transition duration-200 hover:text-teal-500">
+          Request Valuation <i class="pi pi-angle-double-right text-base align-text-bottom text-teal-500"></i>
+        </a>
+      </div>
+    </div>
+      
+  </div> <!--end navbar-->
+
+</div> <!--end container-->
+
+<div class="bg-[url('https://elevation-mk.website-build.info/wp-content/uploads/2024/04/MKHomeBanner.jpg')] bg-cover h-96">
+</div>
+
+
+
+  
 </template>
 
 <style scoped>
